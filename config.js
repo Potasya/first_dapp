b8f07f24f28e7feee7e708920f8a65c37b27f49d
Potasya <yakovlevamava@gmail.com>
2022-12-03 14:08:06 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,31 @@
+const gOptions = {
+    address: "0xb97764BC1F0050662D18fF26eb54DDF94E26D1DD",
+    ABI: [
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_mood",
+                    "type": "string"
+                }
+            ],
+            "name": "setMood",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getMood",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+}
